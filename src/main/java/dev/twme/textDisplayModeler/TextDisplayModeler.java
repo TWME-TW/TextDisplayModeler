ac92470af9bbd014c20e1a8227e4d72e3a40406d
TWME <65117253+TWME-TW@users.noreply.github.com>
2026-05-02 22:43:20 +0800

    feat: initialize project structure for TextDisplayModeler plugin

---
+++ src/main/java/dev/twme/textDisplayModeler/TextDisplayModeler.java
@@ -0,0 +1,16 @@
+package dev.twme.textDisplayModeler;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TextDisplayModeler extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
